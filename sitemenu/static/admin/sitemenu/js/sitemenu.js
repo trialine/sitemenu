98b32686416b42409207a2e8d052f8eeec42c902
Vitālijs Gaičuks <vitalijs.gaicuks@gmail.com>
2022-11-29 17:18:34 +0200

    Call droppable and draggable only if needed

--- sitemenu/static/admin/sitemenu/js/sitemenu.js
+++ sitemenu/static/admin/sitemenu/js/sitemenu.js
@@ -22,55 +22,59 @@ $(function() {
     var target_element = null;
     var element = null;
 
-    $(".drag_handle").draggable({
-        helper: function(){
-            return $('<div></div>');
-        },
-        start: function(event, ui) {
-            $(this).closest('tbody').addClass('drag_started');
-            element = $(this).closest('tr');
-            save_position.element_id = get_id(element);
-            element.addClass('dragging_element');
-        },
-        drag: function(event, ui) {
-            set_level(ui.position.left);
-        },
-        stop: function(event, ui){
-            $(this).closest('tbody').removeClass('drag_started');
-            element.removeClass('dragging_element');
-            element.css('opacity', '1.0');
-        }
-    });
-
-    $( ".catcher" ).droppable({
-        hoverClass: "ui-state-active",
-        drop: function( event, ui ) {
-            if(save_position.target_id != save_position.element_id){
-                console.log(save_position);
-                save();
-            }
-        },
-        over: function( event, ui ) {
-
-            var top_half = false;
-            if($(this).hasClass('catcher_before')){
-                top_half = true;
-            }
-            target_element = $(this).closest('tr');
-            target_level = $('.result_list__ident_span', target_element).length;
-
-            save_position.target_id = get_id(target_element);
-            save_position.top_half = top_half?1:0;
-
-            set_level(0);
-
-            if(save_position.target_id != save_position.element_id){
-                element.css('opacity', '0.5');
-            } else {
+    if ($(".drag_handle").length) {
+        $(".drag_handle").draggable({
+            helper: function(){
+                return $('<div></div>');
+            },
+            start: function(event, ui) {
+                $(this).closest('tbody').addClass('drag_started');
+                element = $(this).closest('tr');
+                save_position.element_id = get_id(element);
+                element.addClass('dragging_element');
+            },
+            drag: function(event, ui) {
+                set_level(ui.position.left);
+            },
+            stop: function(event, ui){
+                $(this).closest('tbody').removeClass('drag_started');
+                element.removeClass('dragging_element');
                 element.css('opacity', '1.0');
             }
-        }
-    });
+        });
+    }
+
+    if ($(".catcher").length) {
+        $(".catcher").droppable({
+            hoverClass: "ui-state-active",
+            drop: function( event, ui ) {
+                if(save_position.target_id != save_position.element_id){
+                    console.log(save_position);
+                    save();
+                }
+            },
+            over: function( event, ui ) {
+
+                var top_half = false;
+                if($(this).hasClass('catcher_before')){
+                    top_half = true;
+                }
+                target_element = $(this).closest('tr');
+                target_level = $('.result_list__ident_span', target_element).length;
+
+                save_position.target_id = get_id(target_element);
+                save_position.top_half = top_half?1:0;
+
+                set_level(0);
+
+                if(save_position.target_id != save_position.element_id){
+                    element.css('opacity', '0.5');
+                } else {
+                    element.css('opacity', '1.0');
+                }
+            }
+        });
+    }
 
     function get_id(el){
         return parseInt($('td.action-checkbox input', el).val(), 10);
